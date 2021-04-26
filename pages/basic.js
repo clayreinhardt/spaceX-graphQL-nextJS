abcc9840d387f198e4183273529a0f315ed051d8
Clay <crp2529@gmail.com>
2021-04-26 10:29:28 -0700

    full dash basic page

---
+++ pages/basic.js
@@ -0,0 +1,18 @@
+import BannerWithImage from '../components/tail/BannerWithImage'
+import BasicNav from '../components/tail/BasicNav'
+import CardAvatarStats from '../components/tail/CardAvatarStats'
+import DashboardSidebar from '../components/tail/DashboardSidebar'
+import FullDash from '../components/tail/FullDash'
+import GridList from '../components/tail/GridList'
+const basic = (props) => {
+  return (
+    // <BasicNav />
+    // <DashboardSidebar />
+    // <BannerWithImage />
+    // <CardAvatarStats />
+    // <GridList />
+    <FullDash />
+  )
+}
+
+export default basic
