4b8713216c9a84f699014b096e694c5bab54b93b
Clay <crp2529@gmail.com>
2021-04-28 11:37:00 -0700

    mobile nav routing to rockets and home now

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -34,7 +34,7 @@ export const Navbar = () => {
                 {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                <Link href="/#">
                <a
-                      className=" border-transparent  hover:border-red-300 hover:text-red-700 inline-flex items-center px-1 pt-1 text-sm font-medium"
+                      className=" border-transparent border-b-2 hover:border-red-300 hover:text-red-700 inline-flex items-center px-1 pt-1 text-sm font-medium"
                 >
                   Home
                 </a>
@@ -169,22 +169,22 @@ export const Navbar = () => {
           <div className="pt-2 pb-4 space-y-1">
             {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
             <a
-              href="#"
+              href="/#"
               className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
             >
-              Dashboard
+              Home
+            </a>
+            <a
+              href="/rockets"
+              className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+            >
+              Rockets
             </a>
             <a
               href="#"
               className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
             >
-              Team
-            </a>
-            <a
-              href="#"
-              className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-            >
-              Projects
+              Coming Soon
             </a>
             <a
               href="#"
