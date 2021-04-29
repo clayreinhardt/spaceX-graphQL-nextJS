66fd39e2ae03020db3f83c9b8c3b6da5a98b96bc
Clay <crp2529@gmail.com>
2021-04-28 17:53:00 -0700

    fixed layout on main page

--- components/Video.jsx
+++ components/Video.jsx
@@ -1,20 +1,27 @@
 import React, {useEffect, useRef} from 'react'
 
 
-const Video = () => {
+const Video = ({url}) => {
 
     const videoRef = useRef()
 
+    const previousUrl = useRef(url);
 
     useEffect(() => {
         setTimeout(()=>{
           videoRef.current.play()
         },5000)
       }, []);
-    
+    useEffect(() => {
+      if (previousUrl.current !== url && videoRef.current) {
+        videoRef.current.load();
+        previousUrl.current = url;
+      }
+    }, [url]);
     return (
         <div>
              <video
+        key={url}
         ref={videoRef}
         controls
         autoPlay
@@ -23,7 +30,8 @@ const Video = () => {
         style={{
           
         }}>
-     <source src="/stars.mp4" type="video/mp4"/>
+     {/* <source src="/stars.mp4" type="video/mp4"/> */}
+          <source src={url} type="url"/>
     </video>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,19 @@ import client from '../apollo-client'
 import Image from 'next/image'
 import recentLaunches from '../components/recentLaunches'
 import Link from 'next/link'
+import dynamic from 'next/dynamic'
+const DynamicVideo = dynamic(import('../components/Video'))
 
 export default function Home({ launches }) {
 
   return (
-    <div className=" w-screen min-h-screen">
+    <div className=" w-full min-h-screen">
       <Head>
         <title>Space X</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className=" flex flex-col space-y-5 py-5 items-center w-screen min-h-screen justify-center px-20 text-center" >
+      <main className=" flex flex-col space-y-5 py-5 items-center w-full min-h-screen justify-center px-20 text-center" >
 
         <h1 className=" backdrop-filter backdrop-blur-sm backdrop-opacity-50 rounded-3xl bg-transparent  text-4xl md:text-6xl font-bold">
           S P A C E - X{' '}<div className="pt-5 text-red-600">L A U N C H E S</div>
@@ -28,8 +30,8 @@ export default function Home({ launches }) {
 
 
 
-          <div className=" w-full border-transparent border-red-400">
-        <div className=" w-screen md:max-w-7xl md:mx-auto md:min-w-5xl flex-wrap justify-center items-center flex-shrink-0 flex ">
+          <div className=" w-full border-transparent border-red-400 mx-auto">
+        <div className=" w-full md:max-w-7xl md:mx-auto md:min-w-5xl flex-wrap justify-center items-center flex-shrink-0 flex ">
             {launches.map((launch, i) => {
               const {
                 // id,
@@ -55,9 +57,10 @@ export default function Home({ launches }) {
                 <a
                   key={i}
                   href={video_link}
-                  className=" backdrop-filter backdrop-brightness-150 backdrop-blur-sm backdrop-opacity-75 shadow-md ease-in-out duration-500 transform scale-90 hover:scale-100 hover:shadow-3xl group flex flex-col items-center justify-center space-y-5 p-6 mt-6 text-left border w-96 rounded-xl hover:text-gray-200 focus:text-red-600"
+                  className=" backdrop-filter backdrop-brightness-150 backdrop-blur-sm backdrop-opacity-75 shadow-md ease-in-out duration-500 transform scale-90 hover:scale-100 hover:shadow-3xl group flex flex-col items-center justify-center space-y-5 p-6 mt-6 text-left border w-auto md:w-96 rounded-xl hover:text-gray-200 focus:text-red-600"
                 >
                   <h3 className="text-2xl transform group-hover:ease-in-out duration-500 font-bold">{mission_name}</h3>
+                  <DynamicVideo url={video_link} />
                   <h4><Link href={`/rockets/${id}`}><a className=" hover:text-blue-500 hover:bg-black">{rocket_name}</a></Link></h4>
                   <Image src={mission_patch ? mission_patch : 'https://images2.imgbox.com/d2/3b/bQaWiil0_o.png'} alt="hi" layout="fixed" height={60} width={60} />
                   <p className=" mt-4 text-xl ease-in-out transform duration-500 w-min relative group-hover:flex group-hover:font-mono ">
