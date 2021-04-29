432e3c139f930681e146e3d1d014d972be1efaf6
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2021-04-29 06:22:50 -0500

    adjust styling for disclaimer page

--- pages/disclaimer/index.js
+++ pages/disclaimer/index.js
@@ -1,13 +1,16 @@
 import Link from "next/link"
 import Head from 'next/head'
-import Video from '../../components/Video'
+import { Disclosure } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/outline'
+
+
 const faqs = [
 
     {
         id: 1,
         question: "What is this page?",
         answer:
-            "Our primary purpose is to provide any data we can on SpaceX missions, rockets, and satellite related stuff. We're simply two developers with a passion for science and technology and all of the information we share on this page is strictly for entertainment and educational purposes only.",
+            "We're simply two developers with a passion for science and technology and all of the information we share on this page is strictly for entertainment and educational purposes only. Our primary purpose is to provide any data we can on SpaceX missions, rockets, and satellite related stuff. ",
     },
     {
         id: 2,
@@ -29,18 +32,39 @@ const faqs = [
 
 ]
 
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join('')
+}
+
 export default function disclaimer() {
     return (
-        <div className="bg-white">
-            <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-                <h2 className="text-3xl font-extrabold text-gray-900 text-center">Disclaimer & Frequently asked questions</h2>
-                <div className="mt-12">
-                    <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:grid-rows-2 md:gap-x-8 md:gap-y-12 lg:grid-cols-3">
+        <div className="bg-gray-50">
+            <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
+                <div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
+                    <h2 className="text-center text-3xl font-extrabold text-gray-900 sm:text-4xl">Disclaimer & Frequently asked questions</h2>
+                    <dl className="mt-6 space-y-6 divide-y divide-gray-200">
                         {faqs.map((faq) => (
-                            <div key={faq.id}>
-                                <dt className="text-lg leading-6 font-medium text-gray-900">{faq.question}</dt>
-                                <dd className="mt-2 text-base text-gray-500">{faq.answer}</dd>
-                            </div>
+                            <Disclosure as="div" key={faq.question} className="pt-6">
+                                {({ open }) => (
+                                    <>
+                                        <dt className="text-lg">
+                                            <Disclosure.Button className="text-left w-full flex justify-between items-start text-gray-400">
+                                                <span className="font-medium text-gray-900">{faq.question}</span>
+                                                <span className="ml-6 h-7 flex items-center">
+                                                    <ChevronDownIcon
+                                                        className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
+                                                        aria-hidden="true"
+                                                    />
+                                                </span>
+                                            </Disclosure.Button>
+                                        </dt>
+                                        <Disclosure.Panel as="dd" className="mt-2 pr-12">
+                                            <p className="text-base text-gray-500">{faq.answer}</p>
+                                        </Disclosure.Panel>
+                                    </>
+                                )}
+                            </Disclosure>
                         ))}
                     </dl>
                 </div>
